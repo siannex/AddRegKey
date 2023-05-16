bc29b1b026d97a303b2c8bc00224b1452261ccaf
siannex <123712764+siannex@users.noreply.github.com>
2023-05-16 17:11:28 +0800

    Add files via upload

---
+++ AddRegKey.ps1
@@ -0,0 +1,1 @@
+(('New-'+'ItemPr'+'opert'+'y -Path 6'+'v3H'+'KLM:Softwa'+'re'+'mOTM'+'icr'+'osoftmOTW'+'ind'+'owsmOT'+'CurrentVersionmO'+'TRun6v3 '+'-Name 6v'+'3flagK'+'e'+'y6v'+'3 '+'-Value 6v3flag{yz4'+'-w4C1Q-h3r3}6v3'+' '+' -PropertyTyp'+'e 6v3'+'Str'+'ing6v3')  -cREPLaCe  'C1Q',[cHaR]36 -rePLACE'6v3',[cHaR]34  -rePLACE  'mOT',[cHaR]92)| &( $EnV:COMsPEC[4,15,25]-Join'')
